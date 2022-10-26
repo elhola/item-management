ff43c7a84e8ee6a1d25a9f8b172335c9130f8fc1
Yaroslav G <yarmcfly@gmail.com>
2022-10-26 19:55:43 +0300

    version 1.0

---
+++ app/src/main/java/com/gmail/yarmcfly/User.java
@@ -0,0 +1,15 @@
+package com.gmail.yarmcfly;
+
+public class User {
+    public String deptname, email;
+
+    public User(){
+
+    }
+
+    public User(String name, String email) {
+        this.deptname = name;
+        this.email = email;
+
+    }
+}
